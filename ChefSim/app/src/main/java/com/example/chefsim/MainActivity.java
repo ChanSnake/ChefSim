2d9b7db7e0c605e55c69d159f808e1ce80eb3dfc
ChanSnake <fearthedarc@gmail.com>
2019-11-21 14:28:43 -0700

    New Activity Load Activity

--- ChefSim/app/src/main/java/com/example/chefsim/MainActivity.java
+++ ChefSim/app/src/main/java/com/example/chefsim/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.chefsim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
+    public void openNew(){
+        Intent newAct = new Intent(this, NewActivity.class);
+        startActivity(newAct);
+    }
+    public void openLoad(){
+        Intent LoadAct = new Intent(this, LoadActivity.class);
+        startActivity(LoadAct);
+    }
+
+    public void clkNew(View view) {
+        openNew();
+    }
+
+    public void clkLoad(View view) {
+        openLoad();
+    }
+
+    public void clkExit(View view) {
+        finish();
+        System.exit(0);
     }
 }
---
+++ ChefSim/app/src/main/java/com/example/chefsim/NewActivity.java
@@ -0,0 +1,22 @@
+package com.example.chefsim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class NewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+
+
+
+
+    public void clk(View view) {
+    }
+}
