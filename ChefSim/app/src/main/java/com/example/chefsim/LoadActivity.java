74e435f9a7a8102d99a81bfe303ed86bcebed345
ChanSnake <fearthedarc@gmail.com>
2019-11-22 09:40:05 -0700

    Load Menu

--- ChefSim/app/src/main/java/com/example/chefsim/LoadActivity.java
+++ ChefSim/app/src/main/java/com/example/chefsim/LoadActivity.java
@@ -2,13 +2,45 @@ package com.example.chefsim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LoadActivity extends AppCompatActivity {
+
+    int speed;
+    int customers;
+    int ingredients;
+    int orderItems;
+    int round;
+    String loadCodeStr;
+    int loadCodeInt;
+
+    EditText load;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+    }
+
+    public void backMain(){
+        Intent newMain = new Intent(this, MainActivity.class);
+        startActivity(newMain);
+    }
+
+    public void clkBack(View view) {
+        backMain();
+    }
+
+    public void clkLoad(View view) {
+        load = findViewById(R.id.etLoad);
+        loadCodeStr = load.getText().toString();
+
+        if(!loadCodeStr.equals(""))
+        {
+            
+        }
     }
 }
